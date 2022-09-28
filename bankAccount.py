d41afd14b24c2737d6b3b3459d2be6d8934d3a5c
Kyle-the-coder <111798115+Kyle-the-coder@users.noreply.github.com>
2022-09-27 20:04:41 -0700

    association between classes

--- bankAccount.py
+++ bankAccount.py
@@ -32,17 +32,46 @@ class BankAccount:
         if self.balance > 0:
             self.balance *= self.rate
         return self 
+
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(int_rate=0.02, balance=0)
+    
+    # other methods
+    def make_withdraw(self):
+        self.account.withdraw(500)
+        return self
+    
+    def make_deposit(self):
+        # your code here
+        self.account.deposit(100)
+        print(self.account)
+        return self	
+
+    def display_info(self):
+        print(self.account)
+        return self
+        
+    
+
+
+    
+
+
         
 
         
-        
+new_user = User("Kyle", "yup@aim.com")        
 
 new_account1 = BankAccount(.01, 1000)
 
 new_account2 = BankAccount(.05, 20)
 
 
-new_account1.deposit(1000).deposit(50).deposit(5).withdraw(1000).yield_interest().display_account_info()
+# new_account1.deposit(1000).deposit(50).deposit(5).withdraw(1000).yield_interest().display_account_info()
 
-new_account2.deposit(100).deposit(100).withdraw(100).withdraw(100).withdraw(5).withdraw(20).yield_interest()
+# new_account2.deposit(100).deposit(100).withdraw(100).withdraw(100).withdraw(5).withdraw(20).yield_interest()
 
+new_user.make_deposit()
